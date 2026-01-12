5d6c005f7ed76709946618d59430585c91d6d398
ARYAN VISHWAKARMA <148914861+ArYaN14138@users.noreply.github.com>
2026-01-12 23:41:29 +0530

    Time: 368 ms (79.86%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -4,3 +4,9 @@ join Weather as w2
 
 on w1.recordDate=DATE_ADD(w2.recordDate,Interval 1 DAY )
 where w1.temperature>w2.temperature;
+
+/*
+SELECT w1.id
+FROM Weather w1, Weather w2
+WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1
+AND w1.temperature > w2.temperature;*/
